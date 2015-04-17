73fd9d1ae99b6dca23df108cf54cfef104b4d007
olufjen@gmail.com <olufjen@gmail.com@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2015-04-17 07:38:59 +0000

    Saksbehandling

--- biovigilans_admin_web/src/main/java/no/naks/biovigilans_admin/web/server/resource/SaksbehandlingServerResourceHTML.java
+++ biovigilans_admin_web/src/main/java/no/naks/biovigilans_admin/web/server/resource/SaksbehandlingServerResourceHTML.java
@@ -4,6 +4,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import no.naks.biovigilans.model.Annenkomplikasjon;
+import no.naks.biovigilans.model.Giverkomplikasjon;
+import no.naks.biovigilans.model.Pasientkomplikasjon;
 import no.naks.biovigilans.model.Vigilansmelding;
 
 import org.restlet.Request;
@@ -18,10 +21,24 @@ import org.restlet.resource.ClientResource;
 import org.restlet.resource.Get;
 import org.restlet.resource.Post;
 
+import freemarker.template.SimpleScalar;
+import no.naks.biovigilans.felles.server.resource.SessionServerResource;
+import no.naks.biovigilans_admin.web.control.SaksbehandlingWebService;
+
 public class SaksbehandlingServerResourceHTML extends SessionServerResource {
 
 	private String meldeKey = "meldinger";
-	
+	protected SaksbehandlingWebService saksbehandlingWebservice;
+
+	public SaksbehandlingWebService getSaksbehandlingWebservice() {
+		return saksbehandlingWebservice;
+	}
+
+	public void setSaksbehandlingWebservice(
+			SaksbehandlingWebService saksbehandlingWebservice) {
+		this.saksbehandlingWebservice = saksbehandlingWebservice;
+	}
+
 	@Get
 	public Representation getHemovigilans() {
 
@@ -37,6 +54,28 @@ public class SaksbehandlingServerResourceHTML extends SessionServerResource {
 	    		melding.setSjekklistesaksbehandling("Levert");
 	    	}
 	    }
+	    Map andreMeldinger = saksbehandlingWebservice.collectAnnenMeldinger(meldinger);
+	    List<Annenkomplikasjon> annenListe =(List) andreMeldinger.get(andreKey);
+	    List<Pasientkomplikasjon> pasientListe = (List) andreMeldinger.get(pasientKey);
+	    List<Giverkomplikasjon> giverListe = (List)  andreMeldinger.get(giverKey);
+	    for (Vigilansmelding melding: meldinger){
+	    	melding.setSupplerendeopplysninger("Ukjent");
+	    	for (Annenkomplikasjon annenKomplikasjon : annenListe){
+	    		if (melding.getMeldeid().longValue() == annenKomplikasjon.getMeldeid().longValue()){
+	    			melding.setSupplerendeopplysninger("Annen hendelse");
+	    		}
+	    	}
+	    	for (Pasientkomplikasjon pasientKomplikasjon : pasientListe){
+	    		if (melding.getMeldeid().longValue() == pasientKomplikasjon.getMeldeid().longValue()){
+	    			melding.setSupplerendeopplysninger("Pasientkomplikasjon");
+	    		}
+	    	}
+	    	for (Giverkomplikasjon giverKomplikasjon : giverListe){
+	    		if (melding.getMeldeid().longValue() == giverKomplikasjon.getMeldeid().longValue()){
+	    			melding.setSupplerendeopplysninger("Giverkomplikasjon");
+	    		}
+	    	}
+	    }	    
 /*
  * En Hashmap benyttes dersom en html side henter data fra flere javaklasser.	
  * Hver javaklasse får en id (ex pasientkomplikasjonId) som er tilgjengelig for html
@@ -87,6 +126,71 @@ public class SaksbehandlingServerResourceHTML extends SessionServerResource {
     	if (meldingsNokkel != null){
     		 meldingDetaljene = (Map<String,List>)saksbehandlingWebservice.selectMeldinger(meldingsNokkel);
     	}
+    	 List<Annenkomplikasjon> annenListe = null;
+    	 List<Pasientkomplikasjon> pasientListe = null;
+    	 List<Giverkomplikasjon> giverListe = null;
+    	 List<Vigilansmelding> meldingen = null;
+    	 Vigilansmelding melding = null;
+    	 Annenkomplikasjon annenKomplikasjon = null;
+    	 Giverkomplikasjon giverKomplikasjon = null;
+    	 Pasientkomplikasjon pasientKomplikasjon = null;
+    	if (meldingDetaljene != null){
+    		meldingen = (List) meldingDetaljene.get(meldingsNokkel);
+    	    annenListe = (List) meldingDetaljene.get(andreKey);
+    	    pasientListe = (List) meldingDetaljene.get(pasientKey);
+    	    giverListe = (List)  meldingDetaljene.get(giverKey);
+    	    if (meldingen != null){
+    	    	melding = meldingen.get(0);
+    	    }
+    	    if (annenListe != null){
+    	    	annenKomplikasjon = annenListe.get(0);
+    	    }
+    	    if (pasientListe != null){
+    	    	pasientKomplikasjon = pasientListe.get(0);
+    	    }
+    	    if (giverListe != null){
+    	    	giverKomplikasjon = giverListe.get(0);
+    	    }
+    	}
+		  String page =  "../hemovigilans/rapporter_andrehendelser.html";
+//  			  setAndreHendelser(); // Setter opp andreHendelser session objekter
+			    // setTransfusjonsObjects(); 
+//  			  annenModel.setFormNames(sessionParams);
+//  		      sessionAdmin.setSessionObject(getRequest(), annenModel,andreHendelseId);
+		  if (annenKomplikasjon != null && melding != null){
+			     setAndreHendelser(); // Setter opp andreHendelser session objekter
+				    // setTransfusjonsObjects(); 
+			     Vigilansmelding minmelding = (Vigilansmelding) annenKomplikasjon;
+			     minmelding.setMeldingsnokkel(melding.getMeldingsnokkel());
+			     minmelding.setMeldingsdato(melding.getMeldingsdato());
+			     minmelding.setDatoforhendelse(melding.getDatoforhendelse());
+			     minmelding.setDatooppdaget(melding.getDatooppdaget());
+			     minmelding.setKladd(melding.getKladd());
+				 annenModel.setFormNames(sessionParams);
+				 annenModel.distributeTerms();
+				 annenModel.setAnnenKomplikasjon(annenKomplikasjon); 
+				 annenModel.setVigilansmelding(melding);
+			   	 annenModel.setHendelseDato(melding.getMeldingsdato());
+		    	 annenModel.setMeldingsNokkel(melding.getMeldingsnokkel());
+		    	 SimpleScalar hendelseDate = new SimpleScalar(datePart);
+		    	 dataModel.put(displaydateKey, hendelseDate);
+				 dataModel.put(andreHendelseId, annenModel);
+				 dataModel.put(annenHendelseId, annenKomplikasjon);
+				 sessionAdmin.setSessionObject(getRequest(), annenModel,andreHendelseId);	
+			  
+ 			  
+		 	
+				 redirectPermanent(page);
+	    	
+			  
+		  }
+
+    	
+    	
+    	
+    	
+    	
+    	
 	     LocalReference pakke = LocalReference.createClapReference(LocalReference.CLAP_CLASS,
               "/hemovigilans");
 	    
