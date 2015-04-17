e079efe41e431f66efcaab60fc46c06f10e379ca
olufjen@gmail.com <olufjen@gmail.com@e7bf3d78-9b82-11dd-8e58-870c635edf7a>
2015-04-17 07:42:20 +0000

    Saksbehandling

--- biovigilans_business/src/main/java/no/naks/biovigilans/service/SaksbehandlingService.java
+++ biovigilans_business/src/main/java/no/naks/biovigilans/service/SaksbehandlingService.java
@@ -1,8 +1,10 @@
 package no.naks.biovigilans.service;
 
 import java.util.List;
+import java.util.Map;
 
 import no.naks.biovigilans.dao.SaksbehandlingDAO;
+import no.naks.biovigilans.model.Vigilansmelding;
 
 public interface SaksbehandlingService {
 	
@@ -12,4 +14,7 @@ public interface SaksbehandlingService {
 	public void setSaksbehandlingDAO(SaksbehandlingDAO saksbehandlingDAO);
 
 	public List collectMessages();
+	public List collectPasientMeldinger(List<Vigilansmelding>meldinger);
+	public List collectGiverMeldinger(List<Vigilansmelding>meldinger);
+	public Map collectAnnenMeldinger(List<Vigilansmelding>meldinger);
 }
