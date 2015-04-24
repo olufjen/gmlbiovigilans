f057f6a3ad9e243122a37f07c95c9ada0eb76b06
olufjen <olufjen@gmail.com>
2015-04-24 10:45:39 +0200

    Oppslag giveropplysninger

--- biovigilans_business/src/main/java/no/naks/biovigilans/model/AbstractKomplikasjonsdiagnosegiver.java
+++ biovigilans_business/src/main/java/no/naks/biovigilans/model/AbstractKomplikasjonsdiagnosegiver.java
@@ -52,53 +52,46 @@ public abstract class AbstractKomplikasjonsdiagnosegiver extends AbstractModel i
 		this.komlikasjonsdiagnoseId = komlikasjonsdiagnoseId;
 	}
 	public String getLokalskadearm() {
-		Map<String,String> userEntries = getKomplikasjonGiverFields();
-		String field = "tab-arm";
-		lokalskadearm = userEntries.get(field);
-		if (lokalskadearm == null ||  lokalskadearm.trim().equalsIgnoreCase("")){
-			lokalskadearm = "";
-		}
-		
+
 		return lokalskadearm;
 	}
 	public void setLokalskadearm(String lokalskadearm) {
-		/*if(lokalskadearm == null){
-			lokalskadearm = komplikasjonGiverFields.get(keys[0]);
-		}*/
+		Map<String,String> userEntries = getKomplikasjonGiverFields();
+		String field = "tab-arm";
+		if (lokalskadearm == null){
+			lokalskadearm = userEntries.get(field);
+			if (lokalskadearm == null ||  lokalskadearm.trim().equalsIgnoreCase("")){
+				lokalskadearm = "";
+			}			
+		}
+
 		this.lokalskadearm = lokalskadearm;
 	}
 	public String getSystemiskbivirkning() {
-		Map<String,String> userEntries = getKomplikasjonGiverFields();
-		String field = "tab-bivirk";
-		systemiskbivirkning = userEntries.get(field);
-		if (systemiskbivirkning == null ){
-			systemiskbivirkning = "";
-		}else{
-			if(systemiskbivirkning.trim().equalsIgnoreCase("Systemisk-Ja")){
-				String systemValue = userEntries.get("tab-systemiskbivirkning");
-				if(systemValue != null || ! systemValue.trim().equalsIgnoreCase("") ){
-					systemiskbivirkning = systemiskbivirkning + ";" + systemValue;
-				}
-			}
-		}
+	
 		return systemiskbivirkning;
 	}
 	public void setSystemiskbivirkning(String systemiskbivirkning) {
-		/*if(systemiskbivirkning ==  null){
-			systemiskbivirkning = komplikasjonGiverFields.get(keys[2]);
-		}*/
+		Map<String,String> userEntries = getKomplikasjonGiverFields();
+		String field = "tab-bivirk";
+		if(systemiskbivirkning ==  null){
+			systemiskbivirkning = userEntries.get(field);
+			if (systemiskbivirkning == null ){
+				systemiskbivirkning = "";
+			}else{
+				if(systemiskbivirkning.trim().equalsIgnoreCase("Systemisk-Ja")){
+					String systemValue = userEntries.get("tab-systemiskbivirkning");
+					if(systemValue != null || ! systemValue.trim().equalsIgnoreCase("") ){
+						systemiskbivirkning = systemiskbivirkning + ";" + systemValue;
+					}
+				}
+			}			
+		}
 		this.systemiskbivirkning = systemiskbivirkning;
 	}
 	public String getAnnenreaksjon() {
 		// OLJ 9.03.15
-		Map<String,String> userEntries = getKomplikasjonGiverFields();
-		String field = "tab-kortbeskr";
-		if (annenreaksjon == null) // Dersom verdien er hentet fra db så skal den ikke være null!?
-			annenreaksjon = userEntries.get(field);
-		if (annenreaksjon == null){
-			annenreaksjon = "";
-		}
-		
+
 		return annenreaksjon;
 	}
 	public void setAnnenreaksjon(String annenreaksjon) {
@@ -111,36 +104,43 @@ public abstract class AbstractKomplikasjonsdiagnosegiver extends AbstractModel i
 		this.annenreaksjon = annenreaksjon;
 	}
 	public String getLokalskadebeskrivelse() {
-		Map<String,String> userEntries = getKomplikasjonGiverFields();
-		String field = "tab-tabskadearm";
-		lokalskadebeskrivelse = userEntries.get(field);
-		if (lokalskadebeskrivelse == null ||  lokalskadebeskrivelse.trim().equalsIgnoreCase("")){
-			lokalskadebeskrivelse = "";
-		}else{
-			
-			if(lokalskadebeskrivelse.trim().equalsIgnoreCase("Nerveirritasjon")){
-				String imgValue = userEntries.get("tab-imgarea");
-				if(imgValue != null){
-					lokalskadebeskrivelse = lokalskadebeskrivelse +";"+imgValue;
-				}
-			}
-		}
+	
+
 		
 		return lokalskadebeskrivelse;
 	}
 	public void setLokalskadebeskrivelse(String lokalskadebeskrivelse) {
-		/*if(lokalskadebeskrivelse == null){
-			lokalskadebeskrivelse = komplikasjonGiverFields.get(keys[1]);
-		}*/
+		Map<String,String> userEntries = getKomplikasjonGiverFields();
+		String field = "tab-tabskadearm";
+		if(lokalskadebeskrivelse == null){
+			lokalskadebeskrivelse = userEntries.get(field);
+			if (lokalskadebeskrivelse == null ||  lokalskadebeskrivelse.trim().equalsIgnoreCase("")){
+				lokalskadebeskrivelse = "";
+			}else{
+				
+				if(lokalskadebeskrivelse.trim().equalsIgnoreCase("Nerveskade/-irritasjon")){
+					String imgValue = userEntries.get("tab-imgarea");
+					if(imgValue != null){
+						lokalskadebeskrivelse = lokalskadebeskrivelse +";"+imgValue;
+					}
+				}
+			}
+		}
 		this.lokalskadebeskrivelse = lokalskadebeskrivelse;
 	}
 	public String getBivirkningbeskrivelse() {
 		return bivirkningbeskrivelse;
 	}
 	public void setBivirkningbeskrivelse(String bivirkningbeskrivelse) {
-		/*if(bivirkningbeskrivelse == null){
-			bivirkningbeskrivelse = komplikasjonGiverFields.get(keys[3]);
-		}*/
+		Map<String,String> userEntries = getKomplikasjonGiverFields();
+		String field = "tab-kortbeskr";
+		if(bivirkningbeskrivelse == null){
+			bivirkningbeskrivelse = userEntries.get(field);
+			if (bivirkningbeskrivelse == null ||  bivirkningbeskrivelse.trim().equalsIgnoreCase("")){
+				bivirkningbeskrivelse = "";
+			}
+			
+		}
 		this.bivirkningbeskrivelse = bivirkningbeskrivelse;
 	}
 	public String getAnnenreaksjonbeskrivelse() {
